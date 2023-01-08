af2ee5f8f46d610d4fbf4ffbbc3a7428a7f6ca90
Andreas G <13819164+agaertner@users.noreply.github.com>
2023-01-08 13:44:51 +0100

    UntrustedCertClientFactory

---
+++ net-utils/_etc/UntrustedCertClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using Flurl;
+using Flurl.Http;
+namespace agaertner.NetUtils
+{
+    public class UntrustedCertClientFactory : DefaultHttpClientFactory
+    {
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            return new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+            };
+        }
+    }
+}
